c5bada6f6e35f2e9390d16ca0b1b4fa0577a763f
Zhong_Ming <a18786717618@126.com>
2020-05-03 19:40:02 +0800

    Simulate the real guitar sound

--- hw1/synthesizer/GuitarString.java
+++ hw1/synthesizer/GuitarString.java
@@ -1,5 +1,4 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-//package <package name>;
+package synthesizer;
 
 //Make sure this class is public
 public class GuitarString {
@@ -14,34 +13,42 @@ public class GuitarString {
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this divsion operation into an int. For better
-        //       accuracy, use the Math.round() function before casting.
-        //       Your buffer should be initially filled with zeros.
+        int capacity = (int) Math.round(SR / frequency);
+        buffer = new ArrayRingBuffer<>(capacity);
+        initGuitarString();
     }
 
+    /**
+     * Initially fill buffer with zeros.
+     */
+    private void initGuitarString() {
+        while (!buffer.isFull()) {
+            buffer.enqueue(0.0);
+        }
+    }
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in the buffer, and replace it with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each other.
+        int cap = buffer.capacity();
+        while (cap > 0) {
+            buffer.dequeue();
+            buffer.enqueue(Math.random() - 0.5);
+            cap--;
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm. 
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       Do not call StdAudio.play().
+        double a = buffer.dequeue();
+        double b = buffer.peek();
+        double avg = (a + b) / 2 * DECAY;
+        buffer.enqueue(avg);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        return buffer.peek();
     }
 }
