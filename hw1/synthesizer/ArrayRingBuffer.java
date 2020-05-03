9743f298037aa48831637d887f5636724f84a6c3
Zhong_Ming <a18786717618@126.com>
2020-05-03 19:38:14 +0800

    Implementation

---
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -0,0 +1,96 @@
+package synthesizer;
+import java.util.Iterator;
+
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
+    /* Index for the next dequeue or peek. */
+    private int first;            // index for the next dequeue or peek
+    /* Index for the next enqueue. */
+    private int last;
+    /* Array for storing the buffer data. */
+    private T[] rb;
+
+    /**
+     * Create a new ArrayRingBuffer with the given capacity.
+     */
+    public ArrayRingBuffer(int capacity) {
+        rb = (T[]) new Object[capacity];
+        this.capacity = capacity;
+        first = 0;
+        last = 0;
+        fillCount = 0;
+    }
+
+    /**
+     * Adds x to the end of the ring buffer. If there is no room, then
+     * throw new RuntimeException("Ring buffer overflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public void enqueue(T x) {
+        if (fillCount() == capacity()) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        last++;
+        if (last == capacity) {
+            last = 0;
+        }
+        fillCount++;
+    }
+
+    /**
+     * Dequeue oldest item in the ring buffer. If the buffer is empty, then
+     * throw new RuntimeException("Ring buffer underflow"). Exceptions
+     * covered Monday.
+     */
+    @Override
+    public T dequeue() {
+        if (fillCount() == 0) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+        T ret = rb[first];
+        rb[first] = null;
+        first++;
+        if (first == capacity) {
+            first = 0;
+        }
+        fillCount--;
+        return ret;
+    }
+
+    /**
+     * Return oldest item, but don't remove it.
+     */
+    @Override
+    public T peek() {
+        return rb[first];
+    }
+
+    /**
+     * Return an iterator.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new BufferIterator();
+    }
+
+    /**
+     * Class implements iterator.
+     */
+    private class BufferIterator implements Iterator<T> {
+        private int ptr;
+        public BufferIterator() {
+            ptr = 0;
+        }
+        @Override
+        public boolean hasNext() {
+            return ptr < capacity();
+        }
+        @Override
+        public T next() {
+            T ret = rb[ptr];
+            ptr++;
+            return ret;
+        }
+    }
+}
