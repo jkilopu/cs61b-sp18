9e263e7392f4e4d0ff8aed4ea2f8714eba845722
Zhong_Ming <a18786717618@126.com>
2020-05-05 01:09:13 +0800

    Basic module

---
+++ proj2/byog/Core/Hallway.java
@@ -0,0 +1,19 @@
+package byog.Core;
+
+import byog.TileEngine.TETile;
+
+import java.util.Random;
+
+public class Hallway {
+
+    Position pos;
+
+    Hallway(Random random) {
+
+    }
+
+    void addHallway(TETile[][] world, Connection cons) {
+
+    }
+
+}
