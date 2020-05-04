2ea0e470c66c3f5daada576c5b5f012c03a4b9fc
Zhong_Ming <a18786717618@126.com>
2020-05-05 01:02:56 +0800

    Complete input analysis, trying to generate the world

--- proj2/byog/Core/Game.java
+++ proj2/byog/Core/Game.java
@@ -2,12 +2,38 @@ package byog.Core;
 
 import byog.TileEngine.TERenderer;
 import byog.TileEngine.TETile;
+import byog.TileEngine.Tileset;
+
+import java.util.Random;
 
 public class Game {
     TERenderer ter = new TERenderer();
+    TETile[][] world;
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
+    private static final Size size = new Size(WIDTH, HEIGHT);
+
+    /**
+     * Constructor
+     */
+    Game() {
+        createEmptyWorld();
+        initWorld();
+    }
+
+    private void createEmptyWorld() {
+        world = new TETile[size.x][size.y];
+        initWorld();
+    }
+
+    private void initWorld() {
+        for (int x = 0; x < size.x; x++) {
+            for (int y = 0; y < size.y; y++) {
+                world[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
 
     /**
      * Method used for playing a fresh game. The game should start from the main menu.
@@ -31,8 +57,71 @@ public class Game {
         // TODO: Fill out this method to run the game using the input passed in,
         // and return a 2D tile representation of the world that would have been
         // drawn if the same inputs had been given to playWithKeyboard().
-
-        TETile[][] finalWorldFrame = null;
+        int option = getOption(input);
+        long seed = Long.parseLong(getSeed(input));
+        Size worldSize = new Size(WIDTH, HEIGHT);
+        switch (option) {
+            case 1:
+                saveSeed(seed);
+                break;
+            case 2:
+                seed = getSeed(file);
+                break;
+        }
+        Random random = new Random(seed);
+        TETile[][] finalWorldFrame = generateWorld(random);
         return finalWorldFrame;
+    }
+
+    private TETile[][] generateWorld(Random random) {
+        Connection cons = new Connection();
+        cons.addConnection(size(), random);
+        for (int i = 0; i < cons.nCon; i++) {
+            Position p = cons.connections[i];
+            Size size = ;
+            Room room = new Room(p, size);
+        }
+        return null;
+    }
+
+    Size size() {
+        return size;
+    }
+
+    /**
+     * Return option specified in input.
+     *
+     * @return 0 means "new game", 1 means "load", 2 means "save"
+     */
+    static int getOption(String input) {
+        String regexLoad = "(?i)^L.*";
+        String regexSave = "(?i).*(:Q)$";
+        if (input.matches(regexLoad))
+            return 1;
+        if (input.matches(regexSave))
+            return 2;
+        return 0; // default: new game;
+    }
+
+    /**
+     * Return the seed found in input.
+     */
+    static String getSeed (String input) {
+        String regexSplit = "(?i)N|S";
+        String []ret = input.split(regexSplit);
+        String regexFind = "[1-9][0-9]*|0";
+        for (String s : ret) {
+            if (s.matches(regexFind))
+                return s;
+        }
+        return "";
+    }
+
+    private void saveSeed() {
+        ;
+    }
+
+    private long getSeed() {
+        return 0;
     }
 }
