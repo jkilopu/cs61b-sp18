ed25df646f210d8d55f591751d6de9596756e30c
Zhong_Ming <a18786717618@126.com>
2020-05-05 15:59:38 +0800

    Use queue to store connections, method addFirstCon's add range decreased

--- proj2/byog/Core/Connection.java
+++ proj2/byog/Core/Connection.java
@@ -2,57 +2,24 @@ package byog.Core;
 
 import byog.TileEngine.TETile;
 
-import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
-class Connection implements Iterable {
-    Position[] connections;
-    int nCon;
-    private static final int MAXNCON = 100;
+class Connection {
+    Queue<Position> connections;
 
     Connection() {
-        nCon = 0;
-        connections = new Position[MAXNCON];
+        connections = new LinkedList();
     }
 
     /**
      * Add a connection which position is limited by s.
      */
-    void addConnection(Size s, Random random) {
-        if (nCon == MAXNCON) {
-            throw new RuntimeException("The number of connections has reached the maximum");
-        }
-        int x = RandomUtils.uniform(random, 0 , s.x);
-        int y = RandomUtils.uniform(random, 0 , s.y);
+    void addFirstCon(Size s, Random random) {
+        int x = RandomUtils.uniform(random, 10, s.x - 10);
+        int y = RandomUtils.uniform(random, 10, s.y - 10);
         Position p = new Position(x, y);
-        connections[nCon] = p;
-        nCon++;
-    }
-
-    @Override
-    public Iterator iterator() {
-        return new ConIterator();
-    }
-
-    /**
-     * Iterator for connections.
-     */
-    private class ConIterator implements Iterator {
-        private int ptr;
-
-        public ConIterator() {
-            ptr = 0;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return ptr != nCon;
-        }
-        @Override
-        public Position next() {
-            Position ret = connections[ptr];
-            ptr++;
-            return ret;
-        }
+        connections.offer(p);
     }
 }
