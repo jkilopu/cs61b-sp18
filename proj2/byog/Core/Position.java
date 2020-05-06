83d48841f725651d9de93273841b47bef81b97ba
Zhong_Ming <a18786717618@126.com>
2020-05-06 09:43:03 +0800

    add equals method

--- proj2/byog/Core/Position.java
+++ proj2/byog/Core/Position.java
@@ -13,6 +13,28 @@ class Position extends Coordinate {
 
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        // 如果为同一对象的不同引用,则相同
+        if (this == obj) {
+            return true;
+        }
+        // 如果传入的对象为空,则返回false
+        if (obj == null) {
+            return false;
+        }
+
+        // 如果两者属于不同的类型,不能相等
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // 类型相同, 比较内容是否相同
+        Position other = (Position) obj;
+
+        return other.x == this.x && other.y == this.y;
+    }
+
     void up() {
         y++;
     }
