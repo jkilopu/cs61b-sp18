842aa6fd91e63c7479681f007afb750dc0d47035
Zhong_Ming <a18786717618@126.com>
2020-05-09 22:30:19 +0800

    Implement Serializable in order to save player information

--- proj2/byog/Core/Coordinate.java
+++ proj2/byog/Core/Coordinate.java
@@ -1,5 +1,15 @@
 package byog.Core;
 
-abstract class Coordinate {
+import java.io.Serializable;
+
+abstract class Coordinate implements Serializable {
     int x, y;
+
+    Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    Coordinate() {
+    }
 }
