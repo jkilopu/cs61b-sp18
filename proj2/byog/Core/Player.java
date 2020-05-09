eb3cc62078ee27e77f32b0fed469f67b4f4544aa
Zhong_Ming <a18786717618@126.com>
2020-05-09 22:31:54 +0800

    Add some notes

--- proj2/byog/Core/Player.java
+++ proj2/byog/Core/Player.java
@@ -3,19 +3,22 @@ package byog.Core;
 import byog.TileEngine.TETile;
 import byog.TileEngine.Tileset;
 
+import java.io.Serializable;
 import java.util.Random;
 
-class Player {
+class Player implements Serializable {
     private Position pos;
     private TETile occupiedTile;
 
     Player() {
         pos = new Position();
-        pos.x = 0;
-        pos.y = 0;
     }
 
-    void getRandomPlayer(TETile[][] world, Random rand) {
+    /**
+     * Set player at proper position (Used when initialize the game)
+     * Violent algorithm.
+     */
+    void setRandomPlayer(TETile[][] world, Random rand) {
         do {
             pos.x = rand.nextInt(world.length);
             pos.y = rand.nextInt(world[0].length);
@@ -23,6 +26,9 @@ class Player {
         } while(world[pos.x][pos.y] != Tileset.FLOOR);
     }
 
+    /**
+     * Add player to the world.
+     */
     void addPlayer(TETile[][] world) {
         if (world[pos.x][pos.y] == Tileset.WALL)
             throw new RuntimeException("Player shouldn't in wall");
@@ -72,13 +78,20 @@ class Player {
         setWorldStatus(world, lastPos);
     }
 
+    /**
+     * Return true if the present position is conflicted.
+     */
     boolean isConflict(TETile[][] world) {
-        if (pos.x >= world.length || pos.x < 0 || pos.y >= world[0].length || pos.y < 0 || world[pos.x][pos.y] != Tileset.FLOOR) {
+        if (pos.x >= world.length || pos.x < 0 ||
+                pos.y >= world[0].length || pos.y < 0 || world[pos.x][pos.y].character() != Tileset.FLOOR.character()) {
             return true;
         }
         return false;
     }
 
+    /**
+     * After One move, set tile in play's previous position.
+     */
     private void setWorldStatus(TETile[][] world, Position lastPos) {
         world[lastPos.x][lastPos.y] = occupiedTile;
         occupiedTile = world[pos.x][pos.y];
