9a7ec3a2f215a020e9ddaf8a5276743e1cbc4e84
Zhong_Ming <a18786717618@126.com>
2020-05-11 11:20:46 +0800

    Monte Carlo simulation

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,79 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
 
+public class PercolationStats {
+    double[] x;
+    int T;
+
+    /**
+     * Constructor, fills x and T.
+     * @param N WorldSize
+     * @param T Try times
+     * @param pf Used to generate percolation
+     */
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("Invalid N or T");
+        }
+        this.T = T;
+        x = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation p = pf.make(N);
+            int[] pos = generateUnrepeatableRandomNumbers(N * N); // All positions with random order.
+            int index = 0;
+            while(!p.percolates()) {
+                int position = pos[index++];
+                int row = position / N, col = position % N;
+                p.open(row, col);
+            }
+            x[i] = (double) p.numberOfOpenSites() / (N * N);
+        }
+    }
+
+    /**
+     * Generate unrepeatable random numbers.
+     * @source https://www.cnblogs.com/happyday56/p/5163264.html
+     */
+    private int[] generateUnrepeatableRandomNumbers(int range) {
+        if (range <= 0) {
+            throw new IllegalArgumentException("Range can't be less than or equal to 0");
+        }
+        int[] source = new int[range];
+        for (int i = 0; i < range; i++) {
+            source[i] = i;
+        }
+
+        int index, len = range;
+        for (int i = 0; i < range; i++) {
+            index = StdRandom.uniform(len--);
+            int tmp = source[index];
+            source[index] = source[len];
+            source[len] = tmp;
+        }
+        return source;
+    }
+
+    public double mean() {
+        return StdStats.mean(x);
+    }                                          // sample mean of percolation threshold
+    public double stddev() {
+        return StdStats.stddev(x);
+    }                                        // sample standard deviation of percolation threshold
+    public double confidenceLow() {
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }                                 // low endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }                                // high endpoint of 95% confidence interval
+
+    /**
+     * Single test.
+     */
+    public static void main(String[] args) {
+        PercolationFactory pf = new PercolationFactory();
+        PercolationStats ps = new PercolationStats(100, 100, pf);
+        System.out.println(ps.mean() + " " + ps.stddev() + "\n" + ps.confidenceLow() + " " +ps.confidenceHigh());
+    }
 }
